e3c03611f100b31080c0b197278147d441fe767e
DeadSky <deadsky.1136@gmail.com>
2025-06-25 05:36:36 +0300

    fix: added default values to optional fields

--- museum_bot/models/registration.py
+++ museum_bot/models/registration.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel
 
 class RegistrationData(BaseModel):
     name: str
+    telegram_id: str
     is_museum_worker: bool = False
-    museum: str | None
-    occupation: str | None
-    telegram_id: str | None
-    tg_username: str | None
-    first_name: str | None
-    last_name: str | None
+    museum: str | None = None
+    occupation: str | None = None
+    tg_username: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
