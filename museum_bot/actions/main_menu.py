33fb4eca0109bd347db25b0e09f392747d2a0622
DeadSky <deadsky.1136@gmail.com>
2025-06-28 03:44:05 +0300

    feat: enhance main menu button filtering by mode
    - Updated the default_main_menu function to include a mode parameter for dynamic button loading based on specified modes.
    - Introduced sorting of buttons by order before rendering to improve menu organization.

--- museum_bot/actions/main_menu.py
+++ museum_bot/actions/main_menu.py
@@ -7,18 +7,19 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from menus import MAIN_MENU
 
 
-async def default_main_menu(message: Message, main_menu_text: str = "Главное меню"):
+async def default_main_menu(message: Message, main_menu_text: str = "Главное меню", *, mode: str = "prod"):
 
     if os.path.exists("templates/main_menu.json"):
         with open("templates/main_menu.json", "r") as f:
             main_menu_data = json.load(f)
 
         if main_menu_data:
+            ordered_buttons = sorted(main_menu_data["buttons"], key=lambda x: x["order"])
             buttons = [
                 InlineKeyboardButton(
                     text=button["text"],
                     callback_data=button["callback_data"]
-                ) for button in main_menu_data["buttons"]
+                ) for button in ordered_buttons if mode in button["modes"]
             ]
             await message.answer(main_menu_text, reply_markup=InlineKeyboardMarkup(inline_keyboard=buttons))
         else:
