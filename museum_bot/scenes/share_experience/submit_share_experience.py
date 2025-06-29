6e3f83ed9387807e6f3c2c8d6c10ba48a91cbbad
DeadSky <deadsky.1136@gmail.com>
2025-06-30 01:22:13 +0300

    fix: update user handling in SubmitShareExperienceScene
    - Modified the on_enter method to accept a User object, ensuring proper user data handling when submitting experiences. This change improves clarity and maintains data integrity by directly using the from_user attribute from the callback.

--- museum_bot/scenes/share_experience/submit_share_experience.py
+++ museum_bot/scenes/share_experience/submit_share_experience.py
@@ -1,5 +1,5 @@
 from aiogram.fsm.scene import Scene, on
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, User
 
 from models.experience import ShareExperienceData
 from services.api_service import send_experience
@@ -8,11 +8,11 @@ from menus import TO_MAIN_MENU_BUTTON
 
 class SubmitShareExperienceScene(Scene, state="share-experience-submit"):
     @on.message.enter()
-    async def on_enter(self, message: CallbackQuery):
+    async def on_enter(self, message: CallbackQuery, from_user: User = None):
         data = await self.wizard.get_data()
 
         valid_data = ShareExperienceData(**{
-            "sm_id": str(message.from_user.id),
+            "sm_id": str(from_user.id),
             **data
         })
 
@@ -27,4 +27,4 @@ class SubmitShareExperienceScene(Scene, state="share-experience-submit"):
     @on.callback_query.enter()
     async def on_enter_callback(self, callback: CallbackQuery):
         await callback.answer()
-        await self.on_enter(callback.message)
+        await self.on_enter(callback.message, callback.from_user)
