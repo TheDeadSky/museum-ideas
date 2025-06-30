26af1bd20ff2f01e3373c4acf3d6417d1437ca15
DeadSky <deadsky.1136@gmail.com>
2025-07-01 02:34:36 +0300

    refactor: update feedback scene to retrieve messages from database and improve user experience

--- museum_bot/scenes/main_menu/feedback_scene.py
+++ museum_bot/scenes/main_menu/feedback_scene.py
@@ -4,28 +4,25 @@ from aiogram.types import Message, CallbackQuery
 from aiogram import F
 
 from menus import CONFIRMATION_MENU, TO_MAIN_MENU_BUTTON
-from services.api_service import send_feedback
+from services.api_service import send_feedback, get_text_from_db
 from models.feedback import Feedback
+from utils import make_one_button_menu
 
 
 class FeedbackScene(Scene, state="feedback"):
     @on.message.enter()
     async def on_enter(self, message: Message):
+        feedback_entry_message = await get_text_from_db("feedback_entry_message")
+
         await message.edit_text(
-            "Введите сообщение для руководителя проекта",
-            reply_markup=TO_MAIN_MENU_BUTTON
+            feedback_entry_message,
+            reply_markup=make_one_button_menu("Отмена", "menu")
         )
 
     @on.callback_query.enter()
     async def on_enter_callback(self, callback_query: CallbackQuery):
         await callback_query.answer()
         await self.on_enter(callback_query.message)
-
-    @on.callback_query(F.data == "cancel")
-    async def cancel_feedback(self, callback_query: CallbackQuery):
-        await callback_query.answer()
-        await callback_query.message.delete_reply_markup()
-        await callback_query.message.edit_text("Отправка отзыва отменена", reply_markup=TO_MAIN_MENU_BUTTON)
 
     @on.message()
     async def handle_feedback(self, message: Message):
@@ -52,7 +49,9 @@ class FeedbackScene(Scene, state="feedback"):
             )
         )
 
-        await callback_query.message.edit_text("Спасибо за ваш отзыв!", reply_markup=TO_MAIN_MENU_BUTTON)
+        feedback_accepted_message = await get_text_from_db("feedback_accepted_message")
+
+        await callback_query.message.edit_text(feedback_accepted_message, reply_markup=TO_MAIN_MENU_BUTTON)
 
     @on.callback_query(F.data == "not_confirm")
     async def not_confirm_feedback(self, callback_query: CallbackQuery):
