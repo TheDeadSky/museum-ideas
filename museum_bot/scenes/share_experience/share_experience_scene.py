ee17231bd17a2981d4619165f6e8e130fa0803d1
DeadSky <deadsky.1136@gmail.com>
2025-06-30 23:12:30 +0300

    refactor: update share experience scene to use CANCEL_BUTTON and add cancel functionality
    - Replaced TO_MAIN_MENU_BUTTON with CANCEL_BUTTON for user input prompts.
    - Implemented a new callback query handler to allow users to cancel and return to the main menu, improving user navigation and experience.

--- museum_bot/scenes/share_experience/share_experience_scene.py
+++ museum_bot/scenes/share_experience/share_experience_scene.py
@@ -2,7 +2,8 @@ from aiogram.fsm.scene import Scene, on, After
 from aiogram.types import Message, CallbackQuery
 from aiogram import F
 
-from menus import TO_MAIN_MENU_BUTTON
+from menus import CANCEL_BUTTON
+from scenes.main_menu.main_menu_scene import MainMenuScene
 from scenes.share_experience.confirmation_scene import ConfirmationScene
 from services.api_service import get_text_from_db
 
@@ -13,7 +14,7 @@ class ShareExperienceScene(Scene, state="share-experience"):
         entry_message = await get_text_from_db("share_experience_entry_message")
         await message.edit_text(entry_message)
         ask_text = await get_text_from_db("share_experience_format_ask")
-        await message.answer(ask_text, reply_markup=TO_MAIN_MENU_BUTTON)
+        await message.answer(ask_text, reply_markup=CANCEL_BUTTON)
 
     @on.callback_query.enter()
     async def on_enter_callback(self, callback: CallbackQuery):
@@ -23,3 +24,8 @@ class ShareExperienceScene(Scene, state="share-experience"):
     @on.message(F.text | F.voice | F.audio, after=After.goto(ConfirmationScene))
     async def handle_message(self, message: Message):
         await self.wizard.update_data(experience=message.text or message.voice or message.audio)
+
+    @on.callback_query(F.data == "cancel", after=After.goto(MainMenuScene))
+    async def cancel(self, callback: CallbackQuery):
+        await callback.answer()
+        await callback.message.delete_reply_markup()
