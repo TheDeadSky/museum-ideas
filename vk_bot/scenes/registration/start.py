41f550eafee10f4e512a0baa9d06a37a24b9a81c
DeadSky <deadsky.1136@gmail.com>
2025-08-25 06:47:03 +0300

    refactor: changed to raw_event system

--- vk_bot/scenes/registration/start.py
+++ vk_bot/scenes/registration/start.py
@@ -1,4 +1,4 @@
-from vkbottle.bot import Message, BotLabeler, rules
+from vkbottle.bot import Message, BotLabeler, rules, MessageEvent
 
 from states.registration import Registration
 from services.api_service import get_text_from_db
@@ -6,11 +6,11 @@ from settings import state_dispenser
 
 
 def init(labeler: BotLabeler):
-    @labeler.message(rules.PayloadRule({"cmd": "registration"}), state=Registration.REGISTRATION_START)
-    async def start_registration(message: Message):
+    @labeler.raw_event(rules.PayloadRule({"cmd": "registration"}), state=Registration.REGISTRATION_START)
+    async def start_registration(event: MessageEvent):
         name_question = await get_text_from_db("name_question")
         state_dispenser.set(
-            message.peer_id,
+            event.peer_id,
             Registration.REGISTRATION_NAME
         )
-        await message.answer(name_question)
+        await event.send_message(name_question)
