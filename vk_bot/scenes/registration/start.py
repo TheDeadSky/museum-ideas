649e00c44ea40e677f6c63c53ae32b76c97e0fe1
DeadSky <deadsky.1136@gmail.com>
2025-08-25 06:50:06 +0300

    fix: some fixes

--- vk_bot/scenes/registration/start.py
+++ vk_bot/scenes/registration/start.py
@@ -1,4 +1,5 @@
-from vkbottle.bot import Message, BotLabeler, rules, MessageEvent
+from vkbottle.bot import BotLabeler, rules, MessageEvent
+from vkbottle_types.events.bot_events import GroupEventType
 
 from states.registration import Registration
 from services.api_service import get_text_from_db
@@ -6,7 +7,12 @@ from settings import state_dispenser
 
 
 def init(labeler: BotLabeler):
-    @labeler.raw_event(rules.PayloadRule({"cmd": "registration"}), state=Registration.REGISTRATION_START)
+    @labeler.raw_event(
+        GroupEventType.MESSAGE_EVENT,
+        MessageEvent,
+        rules.PayloadRule({"cmd": "registration"}),
+        state=Registration.REGISTRATION_START
+    )
     async def start_registration(event: MessageEvent):
         name_question = await get_text_from_db("name_question")
         state_dispenser.set(
