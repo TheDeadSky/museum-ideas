16cf9e4ce25b0ddc5203f7eba018d676ec9a3e3a
DeadSky <deadsky.1136@gmail.com>
2025-09-01 01:37:18 +0300

    fix: added JoinOnlineMeetup scene to registry

--- museum_bot/scenes/get_support/__init__.py
+++ museum_bot/scenes/get_support/__init__.py
@@ -1,5 +1,7 @@
 from aiogram import Router, F
 
+from scenes.get_support.join_online_meetup import JoinOnlineMeetup
+
 from .get_support_scene import GetSupportScene
 from .self_support_course_scene import SelfSupportCourseScene
 from .show_colleagues_stories import ShowColleaguesStoriesScene
@@ -9,6 +11,7 @@ get_support_router = Router()
 get_support_router.callback_query.register(GetSupportScene.as_handler(), F.data == "get_support")
 get_support_router.message.register(GetSupportScene.as_handler(), F.text.casefold() == "получить поддержку")
 get_support_router.callback_query.register(SelfSupportCourseScene.as_handler(), F.data == "self_support_course")
+get_support_router.callback_query.register(JoinOnlineMeetup, F.data == "join_online_meetup")
 
 get_support_scenes_registry = (
     GetSupportScene,
