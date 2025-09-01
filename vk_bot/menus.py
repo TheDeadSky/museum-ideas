69b6816747d10a210b120e950cd67af99c191153
DeadSky <deadsky.1136@gmail.com>
2025-09-01 03:42:33 +0300

    fix: set type callback to all menus and buttons

--- vk_bot/menus.py
+++ vk_bot/menus.py
@@ -25,30 +25,38 @@ MAIN_MENU = Keyboard(one_time=False, inline=True).schema([
 ])
 
 TO_MAIN_MENU_BUTTON = Keyboard(one_time=False, inline=True).schema([
-    [KeyboardButtonSchema(label="В главное меню", payload={"cmd": "main_menu"}).secondary().get_json()],
+    [KeyboardButtonSchema(
+        label="В главное меню",
+        payload={"cmd": "main_menu"},
+        type="callback"
+    ).secondary().get_json()],
 ])
 
 GET_SUPPORT_MENU = Keyboard(one_time=False, inline=True).schema([
     [KeyboardButtonSchema(
         label="Пройти курс самоподдержки",
-        payload={"cmd": "self_support"}
+        payload={"cmd": "self_support"},
+        type="callback"
     ).primary().get_json()],
     [KeyboardButtonSchema(
         label="Узнать истории коллег",
-        payload={"cmd": "colleagues_stories"}
+        payload={"cmd": "colleagues_stories"},
+        type="callback"
     ).primary().get_json()],
 ])
 
 ONE_MORE_STORY_BUTTON = Keyboard(one_time=False, inline=True).schema([
     [KeyboardButtonSchema(
         label="Узнать еще одну историю",
-        payload={"cmd": "one_more_story"}
+        payload={"cmd": "one_more_story"},
+        type="callback"
     ).primary().get_json()],
 ])
 
 NEXT_PART_BUTTON = Keyboard(one_time=False, inline=True).schema([
     [KeyboardButtonSchema(
         label="Смотреть следующую лекцию",
-        payload={"cmd": "self_support_next_part"}
+        payload={"cmd": "self_support_next_part"},
+        type="callback"
     ).primary().get_json()],
 ])
