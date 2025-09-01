16289ae654bf227cb500f5cfec44a053a37d93df
DeadSky <deadsky.1136@gmail.com>
2025-09-01 04:39:34 +0300

    fix: splitted anonymity handlers

--- vk_bot/scenes/share_experience/anonymity.py
+++ vk_bot/scenes/share_experience/anonymity.py
@@ -18,14 +18,32 @@ experience_anonymity_labeler = BotLabeler()
     rules.PayloadRule({
         "cmd": "yes",
         "state": ShareExperienceStates.ANONYMITY.value
-    }),
+    })
+)
+async def handle_anonymous_yes(event: MessageEvent):
+    logging.info("anonymity yes")
+    state_payload = await get_state_payload(state_dispenser, event.peer_id)
+    state_payload.update({
+        "is_anonymous": event.payload == "yes"
+    })
+    await state_dispenser.set(
+        event.peer_id,
+        ShareExperienceStates.ANONYMITY,
+        **state_payload
+    )
+    await submit_share_experience(event, state_payload)
+
+
+@experience_anonymity_labeler.raw_event(
+    GroupEventType.MESSAGE_EVENT,
+    MessageEvent,
     rules.PayloadRule({
         "cmd": "no",
         "state": ShareExperienceStates.ANONYMITY.value
     })
 )
-async def handle_anonymous(event: MessageEvent):
-    logging.info("anonymity yes")
+async def handle_anonymous_no(event: MessageEvent):
+    logging.info("anonymity no")
     state_payload = await get_state_payload(state_dispenser, event.peer_id)
     state_payload.update({
         "is_anonymous": event.payload == "yes"
