31330f05bb528ef213abda927dcd0be40756f82f
DeadSky <deadsky.1136@gmail.com>
2025-09-07 16:33:29 +0300

    debug: testing to add attachment as clip

--- vk_bot/handlers/commands/main_commands.py
+++ vk_bot/handlers/commands/main_commands.py
@@ -40,10 +40,10 @@ async def menu_handler(message: Message):
 
 @commands_labeler.message(command="test_vid")
 async def test_vid_handler(message: Message):
-    video = await video_uploader.upload(
-        file_source="https://ideasformuseums.com/botimages/video/course-1_lecture-1.mp4",
-        peer_id=message.peer_id,
-        owner_id=-229734251
-    )
+    # video = await video_uploader.upload(
+    #     file_source="https://ideasformuseums.com/botimages/video/course-1_lecture-1.mp4",
+    #     peer_id=message.peer_id,
+    #     owner_id=-229734251
+    # )
     await message.answer("🎥 Видео:")
-    await message.answer(attachment=video)
+    await message.answer(attachment="clip-229734251_456239028")
